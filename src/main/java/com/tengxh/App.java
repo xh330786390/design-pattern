f395348b8ca5d4ed5b0c7fc54d5e5806f71047c3
tengxh <330786390@qq.com>
2018-08-01 16:27:49 +0800

    创建项目：DesignPattern

---
+++ src/main/java/com/tengxh/App.java
@@ -0,0 +1,13 @@
+package com.tengxh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
